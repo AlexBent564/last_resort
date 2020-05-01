45206e8d8431e4e4c79acd151f0e55287e7e0e34
IM8065 <60202215+IM8065@users.noreply.github.com>
2020-05-01 12:10:58 -0700

    booking app
    I added my booking part

---
+++ lib/screens/bookHotel/thank_you_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class ThankYouPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: pageColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Thank You For Booking With Us!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 60.0,
+              ),
+
+            ),
+            RaisedButton(
+              child: Text('OK'),
+              onPressed: (){
+                Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
